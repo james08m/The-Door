f88a92a21844593977fe9fc768ca01dfe033a89e
James <james08major@gmail.com>
2018-12-09 08:32:41 -0500

    Add log lvl before printed message.

--- server.py
+++ server.py
@@ -32,37 +32,37 @@ class Server(threading.Thread):
 
     # Safely close the server
     def close(self):
-        print "\rClosing server.."
+        print "\r[i] Closing server.."
         self.servo.idle()                           # Put back servo motor in idle position for security reason ;)
         self.servo.close()                          # Properly close the servo motor instance
 
         self.cleaner.close()                        # Stop server cleaner thread
         self.cleaner.join()                         # Wait that the cleaner thread end
         self.cleaner.kill()                         # Kill remaining clients threads
-        print "Cleaner closed."
+        print "[i] Cleaner closed."
 
         self.server.close()
-        print "Server closed."
+        print "[i] Server closed."
 
     # Redefinition of the run() method from
     # the parent class "threading.Thread".
     # This method is actually the server's
     # thread life cycle
     def run(self):
-        print "Starting server.."
+        print "[i] Starting server.."
 
         self.server.listen(1)  # Start listening
         self.cleaner.start()   # Start client cleaner thread
         time.sleep(0.5)        # Wait half a second to le thread print message
 
-        print "Server started."
-        print "Waiting for connections.."
+        print "[i] Server started."
+        print "[i] Waiting for connections.."
         try:
             while self.alive:
 
                 soc, addr = self.server.accept()            # Wait for a client connection
 
-                print "New connection from : ", addr
+                print "[i] New connection from : ", addr
                 client_id = len(self.clients)
                 client_thread = Client(client_id, addr, soc, self.servo)    # Create a new client instance
                 self.clients.append(client_thread)                          # Add client instance to the clients List
